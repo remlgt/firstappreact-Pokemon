b6f3728aa20f01d02e27649871a44b4b9bd7ac2a
remlgt <remi.loigerot41@gmail.com>
2022-09-08 16:04:14 +0200

    reactJ1

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import react & function component
+  
+ const App: React.FC = () => {
+ const name: String = 'React';
+    
+ return (
+  <h1>Hello, {name} !</h1>
+ )
+}
+  
+export default App;
